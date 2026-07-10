e45f8213268d3c7f0abad687576edd002f79b766
Sharan-0-dot <sharansc482@gmail.com>
2026-07-10 21:35:39 +0530

    Added a landing page for better demonstration

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { UserProvider } from './context/UserContext'
 import { ToastProvider } from './hooks/useToast'
 import IdentityGate from './components/identity/IdentityGate'
 import AppShell from './components/layout/AppShell'
+import LandingPage from './components/shared/LandingPage'
 import ChatPage from './components/agent/ChatPage'
 import JobsListPage from './components/jobs/JobsListPage'
 import JobDetailPage from './components/jobs/JobDetailPage'
@@ -15,20 +16,31 @@ function App() {
   return (
     <UserProvider>
       <ToastProvider>
-        <IdentityGate>
-          <BrowserRouter>
-            <Routes>
-              <Route element={<AppShell />}>
-                <Route index element={<ChatPage />} />
-                <Route path="jobs" element={<JobsListPage />} />
-                <Route path="jobs/:id" element={<JobDetailPage />} />
-                <Route path="profile" element={<ProfilePage />} />
-                <Route path="resume" element={<ResumeUploadPage />} />
-                <Route path="*" element={<NotFoundPage />} />
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </IdentityGate>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+
+            <Route
+              path="/app/*"
+              element={
+                <IdentityGate>
+                  <Routes>
+                    <Route element={<AppShell />}>
+                      <Route index element={<ChatPage />} />
+                      <Route path="jobs" element={<JobsListPage />} />
+                      <Route path="jobs/:id" element={<JobDetailPage />} />
+                      <Route path="profile" element={<ProfilePage />} />
+                      <Route path="resume" element={<ResumeUploadPage />} />
+                      <Route path="*" element={<NotFoundPage />} />
+                    </Route>
+                  </Routes>
+                </IdentityGate>
+              }
+            />
+
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </BrowserRouter>
         <ToastContainer />
       </ToastProvider>
     </UserProvider>
